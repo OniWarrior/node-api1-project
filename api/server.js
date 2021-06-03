d48de4c0abb761cb6e69db759eef3b93b081ee29
Stephen Aranda <aranda.stephen88@gmail.com>
2021-06-02 20:44:19 -0500

    completed first get method to retrieve specific user

--- api/server.js
+++ api/server.js
@@ -1,3 +1,29 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users/:id',(req,res)=>{
+    const idVar = req.params.idVar
+    User.findById(idVar)
+    .then(user=>{
+        if(!user)
+        {
+            res.status(404).json('User does not exist')
+        }
+        else
+        {
+            res.json(user)
+        }
+    })
+    .catch(err =>{
+        res.status(500).json({message:err.message})
+    })
+} )
+
+
+
+ // EXPORT YOUR SERVER instead of {}
